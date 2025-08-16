cb5047aefc33be82f5413115868d4e5bbe998891
Kjshawon <kazimdsolaiman@gmail.com>
2025-08-16 20:00:43 +0600

    2nd

--- P2.js
+++ P2.js
@@ -1,7 +1,10 @@
+// 2. onlyCharacter ফাংশন - স্ট্রিং প্রসেসিং করে
 function onlyCharacter(str) {
+    // ইনপুট স্ট্রিং কিনা চেক
     if (typeof str !== "string") {
         return "Invalid";
     }
+    // সব স্পেস রিমুভ করে এবং বড় হাতের অক্ষরে কনভার্ট করে
     return str.replace(/\s/g, "").toUpperCase();
 }
-console.log(onlyCharacter(" ha ck m e 1 @ru.c  n  ")); // "HAACKME1@RUCN"
+console.log(onlyCharacter(" ha ck m e 1 @ru.c  n  "));
